a4ebe5ccaae0abadc842f19001355c1a4c53705d
Garrett Hale <gert@gert.us>
2020-02-19 10:39:34 -0600

    trim whitespace from both ends

--- library.hpp
+++ library.hpp
@@ -1,15 +1,32 @@
 #pragma once
 
-inline std::string trim_whitespace (const std::string & in)
+inline std::string trim_front_whitespace (const std::string & in)
 {
 	unsigned index {0};
 	while (index < in.length() and std::isspace (in [index]))
-		index++;
+		++index;
 
 	if (index >= in.length() or index == 0)
 		return in;
 
 	return in.substr (index);
+}
+
+inline std::string trim_back_whitespace (const std::string & in)
+{
+	long unsigned index {in.length()-1};
+	while (index != 0 and std::isspace (in [index]))
+		--index;
+
+	if (index >= in.length()-1 or index == 0)
+		return in;
+
+	return in.substr (0, index+1);
+}
+
+inline std::string trim_whitespace (const std::string & in)
+{
+	return trim_back_whitespace (trim_front_whitespace (in));
 }
 
 inline void to_lower_inline (std::string & in)
